0291a02c43f1b76c37c4709bea2b2dc71995f6b5
IdoNaor1 <idonaor15.do@gmail.com>
2024-08-30 12:15:28 +0000

    added MongoDB server, database.js, flower model and files structure

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import connectDB from './database.js';
+import connectDB from './config/database.js';
 import Flower from './models/Flower.js';
 
 export const app = express();
