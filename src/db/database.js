d2959eae9541e4e6cea2714a4bba6ea8f44fd2b5
dor <dorns2122@gmail.com>
2024-09-02 19:59:07 +0300

    Updated db mongoose

--- src/db/database.js
+++ src/db/database.js
@@ -1,19 +1,14 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import { config } from '../config/config.js';
 
-dotenv.config();
+const { mongoUrl } = config;
 
-const connectDB = async () => {
+export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoUrl);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
-
-export default connectDB;
