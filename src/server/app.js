e1564b10f4efe0c0e727729d9f9630a5b1a34f91
IdoNaor1 <idonaor15.do@gmail.com>
2024-09-22 19:08:38 +0000

    Structural changes regarding front and back separation Established front and back connection Added views routes 404 Error Handling

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,38 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { registerAllRoutes } from '../routes/index.js';
+import ejs from 'ejs';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const app = express();
 
+
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+
+app.set('views', path.join(__dirname, '..', '..', 'public', 'views'));
+
 await registerAllRoutes(app);
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+  
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+  
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+
+app.use((req, res) => {
+    res.status(404).render('404');
+});
